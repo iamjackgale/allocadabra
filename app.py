ed99143a71851d9953b8680792975000006abe22
jackgale.eth <jack.gale@icloud.com>
2026-04-26 14:24:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
+from frontend.app import main
 
-import os
-
-# Set environment variables for Streamlit
-os.environ['STREAMLIT_SERVER_HEADLESS'] = 'true'
-os.environ['STREAMLIT_SERVER_PORT'] = os.environ.get('PORT', '8501')
-os.environ['STREAMLIT_SERVER_RUN_ON_SAVE'] = 'false'
-
-from streamlit.web.server import Server
-
-app = Server("frontend/app.py", is_hello=False).app
+if __name__ == "__main__":
+    main()
